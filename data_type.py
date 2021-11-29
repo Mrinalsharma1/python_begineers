9df2b994b8b148cafe7572b97712e4a9202d3c68
Your Name <you@example.com>
2021-11-29 19:45:56 +0530

    learn about list and tupple

---
+++ data_type.py
@@ -0,0 +1,19 @@
+# example of comples
+x = 1j
+print(x)
+print(type(x))
+
+# example of list
+x = ["apple", "boy", "cat"]
+print(x)
+print(type(x))
+
+# example of tupple
+x = ("apple", "boy", "cat")
+print(x)
+print(type(x))
+
+# example of set
+x = {"apple", "boy", "cat"}
+print(x)
+print(type(x))
