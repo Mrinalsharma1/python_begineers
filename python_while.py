85f2bb1c8c4ac182728ea3fae6f8c02b56a57799
Your Name <you@example.com>
2021-12-11 18:37:50 +0530

    today i did learn about condictional statement and while loop

---
+++ python_while.py
@@ -0,0 +1,22 @@
+# while loop
+i = 0
+while i < 5:
+    print(i)
+    i=i+1
+
+# whilw loop with condiction
+i =1
+while i<= 5:
+    if i%2 == 0:
+        print("number is even")
+    else:
+        print("number is odd")
+    i = i+1
+
+# add upto 100 number
+i = 0
+sum = 0
+while i<100:
+    sum = sum+i
+    i=i+1
+print(sum)
